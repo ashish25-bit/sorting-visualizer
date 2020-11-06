5c3c9bccd2b317d60b569a1ea7b463fea3633f6e
ashish yoel <ashishyoel23@gmail.com>
2020-11-06 12:59:09 +0530

    adding walls on the grid.

--- src/components/GraphNodes.js
+++ src/components/GraphNodes.js
@@ -1,27 +1,53 @@
 import React from 'react';
 
 const GraphNodes = ({
-    index,
+    data,
     addType,
-    startingNode,
-    destinationNode
+    onMouseDown,
+    onMouseUp,
+    onMouseEnter
 }) => {
 
-    const className = startingNode ? ( 
-        'node start-node' ) : (
-        destinationNode ? (
-            'node end-node' 
+    const {
+        index,
+        isStart,
+        isEnd,
+        // distance,
+        // isVisisted,
+        isWall,
+        // previousNode,
+    } = data;
+
+    const className = 
+        isStart ? (
+            'node start-node'
+        ) : (
+        isEnd ? (
+            'node end-node'
+        ) : (
+        isWall ? (
+                'node wall-node'
         ) : (
             'node'
-        )
+        ))
     );
 
     return (
         <div 
             className={className}
             onClick={() => addType(index)}
+            onMouseDown={() => onMouseDown(index)}
+            onMouseUp={() => onMouseUp()}
+            onMouseEnter={() => onMouseEnter(index)}
         ></div>
     )
 }
 
 export default GraphNodes;
+
+// {
+//     index,
+//     addType,
+//     startingNode,
+//     destinationNode
+// }
--- src/routes/PathFinding.js
+++ src/routes/PathFinding.js
@@ -1,25 +1,32 @@
-import React, { useLayoutEffect, useState } from 'react';
+import React, { useLayoutEffect, useState, Fragment, useEffect } from 'react';
 import MethodHeader from '../components/MethodHeader';
 import GraphNodes from '../components/GraphNodes';
 import { width80 } from '../utils/exportStyles';
 
+const totalNodes = 800;
+const startNodeindex = 255;
+const destinationNodeIndex = 441;
+
 const PathFinding = () => {
 
     useLayoutEffect(() => { document.title="Path Finding Algorithm" } , []);
-    const totalNodes = 1550;
 
     const [isSelecting, setIsSelecting] = useState(0);
-    const [startingNode, setStartingNode] = useState(null);
-    const [destinationNode, setDestinationNode] = useState(null);
+    const [startingNode, setStartingNode] = useState(startNodeindex);
+    const [destinationNode, setDestinationNode] = useState(destinationNodeIndex);
+    const [nodes, setNodes] = useState([]);
+    const [isMouseClicked, setIsClicked] = useState(false);
+
+    useEffect(() => {
+        const intialSetup = getNodes(startingNode, destinationNode);
+        setNodes(intialSetup);
+    }, [startingNode, destinationNode]);
 
     const selectingNodes = (e, key) => {
         setIsSelecting(key);
     };
 
     const addType = index => {
-
-        if (!isSelecting) 
-            return alert('Select Something');
 
         if (isSelecting === 1) {
             
@@ -40,47 +47,102 @@ const PathFinding = () => {
         }
     }
 
-    return (
-        <div style={{ ...width80, padding: "5px 0" }}>
-            <MethodHeader />
-            <div className="header-path-finding">
+    const mouseDownFunction = index => {
+        const alteredNodes = getAlertedNodes(nodes, index);
+        setNodes(alteredNodes);
+        setIsClicked(true);
+    }
 
-                <button 
-                    onClick={e => selectingNodes(e, 1)}
-                    disabled={isSelecting === 1}
-                >{
-                    isSelecting === 1 ? 
-                        "Selecting Start Point" : 
-                        "Add Starting Point"
-                }</button>
-                
-                <button 
-                    onClick={e => selectingNodes(e, 2)}
-                    disabled={isSelecting === 2}
-                >{
-                    isSelecting === 2 ? 
-                        "Selecting Destination Point" : 
-                        "Add Desination Point"
-                }</button>
+    const mouseUpFunction = () => setIsClicked(false);
+
+    const mouseEnterFunction = index => {
+        if (!isMouseClicked)
+            return;
+
+        const alteredNodes = getAlertedNodes(nodes, index);
+        setNodes(alteredNodes);
+    }
+
+    const clearGrid = () => {
+        const intialSetup = getNodes(startingNode, destinationNode);
+        setNodes(intialSetup);
+    }
+
+    return (
+        <Fragment>
+            <div style={{ ...width80, padding: "5px 0" }}>
+                <MethodHeader />
+                <div className="header-path-finding">
+
+                    <button 
+                        onClick={e => selectingNodes(e, 1)}
+                        disabled={isSelecting === 1}
+                    >{
+                        isSelecting === 1 ? 
+                            "Changing Start Point" : 
+                            "Change Starting Point"
+                    }</button>
+                    
+                    <button 
+                        onClick={e => selectingNodes(e, 2)}
+                        disabled={isSelecting === 2}
+                    >{
+                        isSelecting === 2 ? 
+                            "Changing Destination Point" : 
+                            "Change Desination Point"
+                    }</button>
+                    <button onClick={clearGrid}>Clear Grid</button>
+                </div>           
+                <div className='legend'>
+                    <div></div>
+                    <div></div>
+                </div>
             </div>
-            
-            {/* <GraphNodes isSelecting={isSelecting} /> */}
+
+
             <div className='path-finding-nodes-container'>
-                {[...Array(totalNodes).keys()].map( idx => {
-                    return (
-                        <GraphNodes
-                            key={idx}
-                            index={idx}
-                            addType={addType}
-                            startingNode={startingNode === idx ? startingNode : null}
-                            destinationNode={destinationNode === idx ? destinationNode : null}
-                        />
-                    )
-                })}
+            {nodes.map((nodeData, index) =>
+                    <GraphNodes 
+                        key={index}
+                        data={nodeData}
+                        addType={addType}
+                        onMouseDown={index => mouseDownFunction(index)}
+                        onMouseUp={() => mouseUpFunction()}
+                        onMouseEnter={() => mouseEnterFunction(index)}
+                    />
+            )}
             </div>
-            
-        </div>
+        </Fragment>
     )
+}
+
+function getNodes(startingNode, destinationNode) {
+    return [...Array(totalNodes).keys()].map(index => createNode(index, startingNode, destinationNode));
+}
+
+const createNode = (index, startingNode, destinationNode) => {
+    return {
+        index,
+        isStart: index === startingNode,
+        isEnd: index === destinationNode,
+        distance: Infinity,
+        isVisisted: false,
+        isWall: false,
+        previousNode: null
+    }
+}
+
+const getAlertedNodes = (nodes, index) => {
+    const newNodes = nodes.slice();
+    const node = newNodes[index];
+
+    const newNode = {
+        ...node, 
+        isWall: !node.isWall
+    }
+
+    newNodes[index] = newNode;
+    return newNodes;
 }
 
 export default PathFinding
